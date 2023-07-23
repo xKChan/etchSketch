33b20c8d67370059198a979858e70d158dbac1a4
kc <xkylechan@gmail.com>
2023-07-23 16:23:48 -0600

    function to create new grid size between 10 - 50

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-const TOTALROWS = 16;
-const TOTALCOLUMNS = 16;
+const gridSize = 16;
 
 
 const container = document.querySelector('#container');
-const reset = document.querySelector('button');
+const reset = document.querySelector('#reset');
+const newGrid = document.querySelector('#newGrid');
 
+
+// Functions
 
 // Function to create a grid
-function createGrid(TOTALCOLUMNS, TOTALROWS) {
+function createGrid(gridSize) {
     // Create grid Columns
-    for (let i = 0; i < TOTALCOLUMNS; i++) {
+    for (let i = 0; i < gridSize; i++) {
         const createGrid = document.createElement('div');
         createGrid.classList.add('cols');
 
 
         // Create grid Rows
-        for (let j = 0; j < TOTALROWS; j++) {
+        for (let j = 0; j < gridSize; j++) {
             const createRows = document.createElement('div');
             createRows.classList.add('rows');
             createGrid.appendChild(createRows);
@@ -24,21 +26,92 @@ function createGrid(TOTALCOLUMNS, TOTALROWS) {
     }
 }
 
-createGrid(TOTALCOLUMNS, TOTALROWS);
+function draw() {
+    const draw = document.querySelectorAll('.rows');
 
-
-// Draw lines, when hovering over div
-const draw = document.querySelectorAll('.rows');
-draw.forEach(row => {
+    draw.forEach(row => {
     row.addEventListener('mouseover', () => {
         row.classList.add('hovered');
     });
 });
+}
 
-reset.addEventListener('click', () => {
-    draw.forEach(row => {
-        row.classList.remove('hovered');
-    })
+function resetGrid() {
+    let resetGridLines = document.querySelectorAll('.rows');
 
-});
+    resetGridLines.forEach(gridItem => {
+        gridItem.classList.remove('hovered');
+    });
+}
 
+function newGridSize() {
+    let x = 0;
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+
+    do {
+        x = prompt('New Grid Size:  (Min. 10 // Max 50');
+    } while (x > 50 || x < 10);
+
+    createGrid(x);
+    draw();
+}
+
+
+
+createGrid(gridSize);
+draw();
+
+
+// Event Listeners for buttons
+reset.addEventListener('click', resetGrid);
+newGrid.addEventListener('click', newGridSize);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function reset() {
+//     reset.addEventListener('click', () => {
+//         draw.forEach(row => {
+//             row.classList.remove('hovered');
+//         })
+//     });
+// }
+
+// newGrid.addEventListener('click', () => {
+//     while (container.firstChild) {
+//         container.removeChild(container.lastChild);
+//     }
+
+//     // Create new grid
+//     let newSize = prompt('Grid Size: ');
+//     createGrid(newSize);
+// });
